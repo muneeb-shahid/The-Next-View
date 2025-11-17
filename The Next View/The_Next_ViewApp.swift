9ecc8280ee2d647e3fc7b1c1839ffd0de094f77d
Muneeb Shahid <muneebshahid0102@gmail.com>
2025-11-17 14:02:45 +0500

    Initial Commit

---
+++ The Next View/The_Next_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Next_ViewApp.swift
+//  The Next View
+//
+//  Created by UTF LABS on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct The_Next_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
